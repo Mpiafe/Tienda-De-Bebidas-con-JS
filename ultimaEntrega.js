67562b533eb0c2396d24b2047d7373aacfcb81ba
Mpiafe <mpiaferreyra@gmail.com>
2023-01-27 08:43:24 -0300

    ok

--- ultimaEntrega.js
+++ ultimaEntrega.js
@@ -62,24 +62,37 @@ function enviarAForm(producto) {
   
 
 }
-/*Funcion para eliminar producto del carrito//
-const vaciarCarrito = document.getElementsByClassName (".icono-eliminar")
-
-vaciarCarrito.addEventListener ("click", (${producto})=>{
-  let productId = producto.id
-  carrito = carrito.filter((producto) => producto.id !== productId)
-  mostrarCarrito()
-})
-
+//Funcion para eliminar producto del carrito//
 function mostrarCarrito (){
+  const contenedorPedido = document.querySelector(".contenedorPedido");
+  contenedorPedido.innerHTML = `<table class="table table-dark table-striped">
 
-}*/
+  <thead class="items">
+  <tr>
+    
+    <th scope="col">Producto</th>
+    <th scope="col">Precio</th>
+    <th scope="col">Cantidad</th>
+    <th scope="col">Total</th>
+    <th scope="col">Eliminar</th>
+  </tr>
+</thead>`
+}
+
+const eliminarItem = (id) => {
+    let borrar = carrito.find((producto) =>  producto.id === id)
+    let indice = carrito.indexOf(borrar)
+    carrito.splice(indice, 1)
+    mostrarCarrito ()
+
+  
+};
+
 
 ///Funcion para pintar tabla////
-
-
 function pintarHtmlPedido(carrito) {
   const contenedorPedido = document.querySelector(".contenedorPedido");
+
   let iconoEliminar = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -97,7 +110,7 @@ function pintarHtmlPedido(carrito) {
       <th scope="col">Precio</th>
       <th scope="col">Cantidad</th>
       <th scope="col">Total</th>
-      <th scope="col"></th>
+      <th scope="col">Eliminar</th>
     </tr>
   </thead>
     <tbody class="bodyprod">
@@ -120,7 +133,6 @@ function pintarHtmlPedido(carrito) {
     contenedorPedido.appendChild(pedidoProducto);
     
   });
-
 
   ///Boton  confirmar pedido///
   const btnPedido= document.getElementById('botonPedido');
